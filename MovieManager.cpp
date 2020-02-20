fffa0ba200ec0009b44e2303c6b4fc887e6dce5f
Anthony Valenzuela <aovalenz@uci.edu>
2020-02-20 09:56:13 -0800

    added while loop in the run function of the MovieManager class

--- MovieManager.cpp
+++ MovieManager.cpp
@@ -12,11 +12,12 @@ void MovieManager::run() {
     MovieManagerUI show;
     show.printMenu();
     std::string command = show.getCommand();
-    if (command == "rm" || command == "RM" || command == "rM" || command == "Rm")
-        {
-            std::cout << "rent movie" << std::endl;
-            
-        }
+    while(command != "q" || command != "Q"){
+        if (command == "rm" || command == "RM" || command == "rM" || command == "Rm")
+            {
+                std::cout << "rent movie" << std::endl;
+                
+            }
 
         if (command == "dm" || command == "DM" || command == "dM" || command == "Dm")
         {
@@ -29,30 +30,34 @@ void MovieManager::run() {
                     arrayOfMovies[i] = obj;
                 }
             }
-           
+            
         }
         if (command == "am" || command == "AM" || command == "aM" || command == "Am")
         {
             // add movie here
             Movies m = show.addMoviePrompt(); 
-            
-            
+            std::cout << "Movie has been added" << std::endl;
+            show.printMenu();
+            std::string command = show.getCommand();
+
         }
         if (command == "rr" || command == "RR" || command == "rR" || command == "Rr")
         {
-            std::cout << "return rental" << std::endl;
-            
+                std::cout << "return rental" << std::endl;
+                
         }
         if (command == "p" || command == "P" )
         {
             std::cout << "print" << std::endl;
-            
+                
         }
         if (command == "q" || command == "Q" )
         {
             std::cout << "quit" << std::endl;
-            
+                
         }
+    }
+    std::cout << "Program terminated" << std::endl;
 }
 
 //adds movie
